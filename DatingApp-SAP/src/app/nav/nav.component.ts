f67527d51444ead2d3d28b34ef7d19bb58d8ccd6
Carlos-Gamer\Carlos <carlos.sierra.chacon@gmail.com>
2019-10-27 13:59:09 +0100

    End section 7

--- DatingApp-SAP/src/app/nav/nav.component.ts
+++ DatingApp-SAP/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/Alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,19 +12,21 @@ export class NavComponent implements OnInit {
 
   model: any ={};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login()
   {
-    this.authService.login(this.model).subscribe( 
+    this.authService.login(this.model).subscribe(
       next => {
         this.alertify.success('logger in successfully');
       },
       error => {
         this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -37,5 +40,7 @@ export class NavComponent implements OnInit {
     localStorage.removeItem('token');
 
     this.alertify.message('logged out');
+
+    this.router.navigate(['/home']);
   }
 }
